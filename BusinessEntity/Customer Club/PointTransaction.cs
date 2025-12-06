802a5804a2cd4a85ce0f432c6ed3e7d28f46c1a7
Reza Parang <rezaparang03@gmail.com>
2025-12-06 11:53:38 +0330

    customer club database

---
+++ BusinessEntity/Customer Club/PointTransaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntity.Customer_Club
+{
+    public class PointTransaction
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public Customer User { get; set; } = null!;
+
+        public int ActivityId { get; set; }
+        public Activity Activity { get; set; } = null!;
+
+        public int Points { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
